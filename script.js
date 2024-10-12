45ca3c282d7566c54faec55d9ee85303e5068c87
Mokete <emtyabekana@gmail.com>
2024-10-12 18:56:41 +0200

    Use document.getElementById to select the task-list and task-input

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 //At the beginning of your script, use document.addEventListener to listen for the 'DOMContentLoaded' event.
 
-document.addEventListener('DOMContentLoaded',function(){
+document.addEventListener("DOMContentLoaded", function () {
 
+  //Use document.getElementById to select the “Add Task” button,task-list and task-input
 
-    
+  const addButton = document.getElementById("add-task-btn");
+  const taskInput = document.getElementById("task-input");
+  const taskList = document.getElementById("task-list");
 });
